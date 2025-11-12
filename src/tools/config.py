768697497a9c02a0e347bcbf50b3e0caac2e8602
thsmale <60244532+thsmale@users.noreply.github.com>
2025-11-11 21:24:43 -0800

    reorganize mcp files into src directory (#5)

---
+++ src/tools/config.py
@@ -0,0 +1,64 @@
+# Tools
+from tools.v2.budget_functions.list_budget_functions import (
+    call_tool_list_budget_functions,
+    tool_list_budget_functions,
+)
+from tools.v2.federal_accounts import (
+    call_tool_federal_accounts,
+    tool_federal_accounts,
+)
+from tools.v2.financial_spending.major_object_class import (
+    call_tool_major_object_class,
+    tool_major_object_class,
+)
+from tools.v2.recipient import (
+    call_tool_recipient,
+    tool_recipient,
+)
+from tools.v2.references.toptier_agencies import (
+    call_tool_toptier_agencies,
+    tool_toptier_agencies,
+)
+from tools.v2.references.total_budgetary_resources import (
+    call_tool_total_budgetary_resources,
+    tool_total_budgetary_resources,
+)
+from tools.v2.search.spending_by_award import (
+    call_tool_spending_by_award,
+    tool_spending_by_award,
+)
+from tools.v2.search.spending_over_time import (
+    call_tool_spending_over_time,
+    tool_spending_over_time,
+)
+from tools.v2.spending import (
+    call_tool_spending,
+    tool_spending,
+)
+from tools.v2.subawards import (
+    call_tool_subawards,
+    tool_subawards,
+)
+
+__all__ = [
+    "call_tool_list_budget_functions",
+    "tool_list_budget_functions",
+    "call_tool_federal_accounts",
+    "tool_federal_accounts",
+    "call_tool_major_object_class",
+    "tool_major_object_class",
+    "call_tool_recipient",
+    "tool_recipient",
+    "call_tool_toptier_agencies",
+    "tool_toptier_agencies",
+    "call_tool_total_budgetary_resources",
+    "tool_total_budgetary_resources",
+    "call_tool_spending_by_award",
+    "tool_spending_by_award",
+    "call_tool_spending_over_time",
+    "tool_spending_over_time",
+    "call_tool_spending",
+    "tool_spending",
+    "call_tool_subawards",
+    "tool_subawards",
+]
